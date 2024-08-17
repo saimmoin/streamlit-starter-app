05a70543667424c18ce3516df4cd80ae21233ca2
Saim Saqib <73789776+saimmoin@users.noreply.github.com>
2024-08-17 10:04:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,10 @@ with st.expander('Accordian'):
   st.write('This is a text which is inside accordian.')
 
 with st.sidebar:
-    sex = st.radio("Select your gender:", ['Male', 'Female', 'Custom'])
+    gender = st.radio("Select your gender:", ['Male', 'Female', 'Custom'])
     age = st.slider("Select your age:", 18, 45)
     profession = st.selectbox("Select your profession:", ('Data Scientist', 'Blockchain Developer', 'React Developer (Frontend)'))
 
-st.write('**Sex**:',sex)
+st.write('**Gender**:', gender)
+st.write('**Age**:', age)
+st.write('**Profession**:', profession)
