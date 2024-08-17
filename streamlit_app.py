e03483671a8d2256b1deb592fb45d7f42a521752
Saim Saqib <73789776+saimmoin@users.noreply.github.com>
2024-08-17 09:36:20 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Welcome to Streamlit starter application🎈')
 
 st.info('**Made by: Saim Saqib**')
+
+st.expander('Accordian'):
+  st.write('This is a text which is inside accordian.')
