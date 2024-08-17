ea785c819d7c7fce0cfc17c52ec8f62022247984
Saim Saqib <73789776+saimmoin@users.noreply.github.com>
2024-08-17 09:53:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,38 +7,3 @@ st.info('**Made by: Saim Saqib**')
 with st.expander('Accordian'):
   st.write('This is a text which is inside accordian.')
 
-with st.sidebar:
-    st.radio("Select one:", [1, 2])
-
-st.button("Click me")
-st.download_button("Download file", data)
-st.feedback("thumbs")
-st.link_button("Go to gallery", url)
-st.page_link("app.py", label="Home")
-st.data_editor("Edit data", data)
-st.checkbox("I agree")
-st.toggle("Enable")
-st.radio("Pick one", ["cats", "dogs"])
-st.selectbox("Pick one", ["cats", "dogs"])
-st.multiselect("Buy", ["milk", "apples", "potatoes"])
-st.slider("Pick a number", 0, 100)
-st.select_slider("Pick a size", ["S", "M", "L"])
-st.text_input("First name")
-st.number_input("Pick a number", 0, 10)
-st.text_area("Text to translate")
-st.date_input("Your birthday")
-st.time_input("Meeting time")
-st.file_uploader("Upload a CSV")
-st.camera_input("Take a picture")
-st.color_picker("Pick a color")
-
-# Use widgets' returned values in variables:
-for i in range(int(st.number_input("Num:"))):
-    foo()
-if st.sidebar.selectbox("I:",["f"]) == "f":
-    b()
-my_slider_val = st.slider("Quinn Mallory", 1, 88)
-st.write(slider_val)
-
-# Disable widgets to remove interactivity:
-st.slider("Pick a number", 0, 100, disabled=True)
