291ab90e94678ecd01145e679299957b33359b41
Saim Saqib <73789776+saimmoin@users.noreply.github.com>
2024-08-18 09:50:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,23 @@ import streamlit as st
 import PyPDF2
 import google.generativeai as genai
 
-def read_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        num_pages = len(pdf_reader.pages)
-        text = ""
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
-        return text
+# def read_pdf(pdf_path):
+#     with open(pdf_path, 'rb') as file:
+#         pdf_reader = PyPDF2.PdfReader(file)
+#         num_pages = len(pdf_reader.pages)
+#         text = ""
+#         for page_num in range(num_pages):
+#             page = pdf_reader.pages[page_num]
+#             text += page.extract_text()
+#         return text
+
+def read_pdf(file):
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    text = ""
+    for page_num in range(pdf_reader.getNumPages()):
+        page = pdf_reader.getPage(page_num)
+        text += page.extract_text()
+    return text
 
 def configure_genai(api_key):
     genai.configure(api_key=api_key)
